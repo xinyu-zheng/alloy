5db207505b02ca0e6db953f8e7048846981fd21c
Jake Hughes <jh@jakehughes.uk>
2024-04-23 17:06:54 +0100

    Run Alloy with GC assertions on CI

--- .buildbot.sh
+++ .buildbot.sh
@@ -14,5 +14,5 @@ export RUSTUP_HOME="`pwd`/.rustup"
 # Ensure the build fails if it uses excessive amounts of memory.
 ulimit -d $((1024 * 1024 * 8)) # 8 GiB
 
-/usr/bin/time -v python3 x.py test --stage 2 --config .buildbot.config.toml --exclude rustdoc-json --exclude debuginfo
+ENABLE_GC_ASSERTIONS=true /usr/bin/time -v python3 x.py test --stage 2 --config .buildbot.config.toml --exclude rustdoc-json --exclude debuginfo
 
